3a817ef2a7fee05f8a9855220b7f81eb9afb3dcb
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-01 13:31:34 +0530

    [Madhav] Add: Divisibility by 5

--- DivisibilityBy5.java
+++ DivisibilityBy5.java
@@ -1,1 +1,21 @@
-code
+// Simple program to check if a number is divisible by 5
+import java.util.Scanner;
+
+class DivisibilityBy5 {
+    public static void main(String[] args) {
+
+        // Create a Scanner to take user input
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        // Check if the number is divisible by 5
+        boolean isDivisible = (number % 5 == 0);
+
+        // Print the result
+        System.out.println("Is the number " + number + " divisible by 5? " + isDivisible);
+    }
+}
+
