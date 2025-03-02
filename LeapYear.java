b7ca38e1c16d7fb1d9400a6168ba5d65a9d0758e
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-02 15:10:36 +0530

    [Madhav] Add: checking for a leap year

---
+++ LeapYear.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter a year (>=1582): ");
+        int year = sc.nextInt();
+
+        // Check for valid year
+        if (year < 1582) {
+            System.out.println("Invalid input! Year must be 1582 or later.");
+        }
+	else {
+
+            // Check leap year conditions using multiple if-else statements
+            if (year % 400 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } 
+		else if (year % 100 == 0) {
+                System.out.println(year + " is not a Leap Year.");
+            } 
+		else if (year % 4 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } 
+		else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        }
+    }
+}
